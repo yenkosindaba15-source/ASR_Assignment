8577697737e2f8845496a008808126aee19b42e6
Ntando <yenkosindaba15@gmail.com>
2026-09-23 01:46:23 +0200

    Carried out camera sensor integration in Webots

--- webots_project/controllers/move_forward/move_forward.py
+++ webots_project/controllers/move_forward/move_forward.py
@@ -1,53 +1,15 @@
 from controller import Robot
 
 robot = Robot()
+
 TIME_STEP = 64
 
-left_motor = robot.getDevice('left wheel motor')
-right_motor = robot.getDevice('right wheel motor')
-
-left_motor.setPosition(float('inf'))
-right_motor.setPosition(float('inf'))
-
-left_motor.setVelocity(3.0)
-right_motor.setVelocity(3.0)
-
-sensor_names = [ #The robot's "eyes".
-    'ps0', 'ps1', 'ps2', 'ps3', 'ps4', 'ps5', 'ps6', 'ps7'
-]
-sensors = []
-
-for name in sensor_names:
-    sensor = robot.getDevice(name)
-    sensor.enable(TIME_STEP)
-    sensors.append(sensor)
+camera = robot.getDevice("camera")
+camera.enable(TIME_STEP)
 
 while robot.step(TIME_STEP) != -1:
-    values = [sensor.getValue() for sensor in sensors]
 
-    front_obstacle = (
-        values[0] > 80 or
-        values[1] > 80 or
-        values[6] > 80 or
-        values[7] > 80
-    )
+    width = camera.getWidth()
+    height = camera.getHeight()
 
-    left_side = values[5]
-    right_side = values[2]
-
-    # Robot makes turning decisions.
-    if front_obstacle:
-        if left_side < right_side:
-            print("TURNING LEFT")
-
-            left_motor.setVelocity(-2.0)
-            right_motor.setVelocity(2.0)
-        else:
-            print("TURNING RIGHT")
-
-            left_motor.setVelocity(2.0)
-            right_motor.setVelocity(-2.0)
-    else:
-        print("MOVING FORWARD")
-        left_motor.setVelocity(3.0)
-        right_motor.setVelocity(3.0)
+    print(f"Camera Active: {width} x {height}")
