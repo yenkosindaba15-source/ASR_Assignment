c4d21affa896d056c43182d0af67841e16b5935b
Ntando <yenkosindaba15@gmail.com>
2026-09-23 01:15:54 +0200

    Created occupancy grid visualization

--- python_scripts/occupancy_grid.py
+++ python_scripts/occupancy_grid.py
@@ -1,9 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 grid = np.zeros((10, 10))
 
+#obstacles
 grid[3][3] = 1
 grid[3][4] = 1
 grid[4][3] = 1
+grid[7][8] = 1
+grid[6][8] = 1
 
-print(grid)
+plt.imshow(grid, cmap='gray_r')
+plt.title("Occupancy Grid Map")
+plt.xlabel("X Position")
+plt.ylabel("Y Position")
+plt.colorbar(label = "Obstacle")
+plt.show()
